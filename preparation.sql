c40297d3c9c75c3426dbebde22457f87da1eb02c
Bourama coulibaly <bouramadevweb@gmail.com>
2024-02-08 04:07:13 +0100

    presque finir

--- preparation.sql
+++ preparation.sql
@@ -1,5 +1,5 @@
---CREATTION DE TABLE CHEESES
-CREATE TABLE  cheeses (
+
+CREATE TABLE cheesess (
     id VARCHAR2(20) NOT NULL,
     fromage VARCHAR2(50) NULL,
     family VARCHAR2(50) NULL,
@@ -8,20 +8,23 @@ CREATE TABLE  cheeses (
     PRIMARY KEY(id)
 );
 
---CREATTION DE TABLE DATES
-CREATE TABLE  dates (
-    id VARCHAR2(50) PRIMARY KEY,
-    sale_date TEXT NOT NULL
+-- Create the ventes table with foreign key constraints
+CREATE TABLE ventes (
+    cheeses VARCHAR2(50),
+    vente_date VARCHAR2(50),
+    quantites NUMBER,
+    PRIMARY KEY (cheeses),
+    CONSTRAINT fk_Ventesdate FOREIGN KEY (vente_date) REFERENCES Ventesdate(id),
+    CONSTRAINT fk_cheesess FOREIGN KEY (cheeses) REFERENCES cheesess(id)
 );
---CREATTION DE TABLE SALES
-CREATE TABLE  Sales (
-    id_sales VARCHAR2(50) PRIMARY KEY,
-    quantity INTEGER NOT NULL,
-    id VARCHAR2(50) NOT NULL,
-    sale_date_id VARCHAR2(50) NOT NULL,
-    FOREIGN KEY (id) REFERENCES D_cheeses(id) ON DELETE CASCADE,
-    FOREIGN KEY (sale_date_id) REFERENCES sale_dates(id) ON DELETE CASCADE
+
+CREATE TABLE sales (
+    cheeses VARCHAR2(50),
+    vente_date VARCHAR2(50),
+    quantites VARCHAR2(5)
+    
 );
+
 
 INSERT INTO cheeses (id, fromage, family, paste, prix) 
 SELECT DISTINCT id, fromage, FAMILY, PASTE, PRICE
@@ -40,26 +43,26 @@ CREATE TABLE ODSFROMAGES (
    image_filename VARCHAR2(50),
    PRIMARY KEY(id)
 );
--- INSERTION DES DONNEES DANS LA TABLE CHEESES
-INSERT INTO cheesess (id, fromage, family, paste, prix) 
-SELECT DISTINCT 
-    REPLACE(TRIM(id || fromage || family || paste), ' ', '') AS id_concatenated, 
-    fromage, 
-    odsfromages.family, 
-    paste, 
-    price AS prix
-FROM ODSFROMAGES;
--- creation de table temporaire vente 
-CREATE TABLE ventes (
-    cheeses VARCHAR2(50),
-    dates DATE,
-    quantites NUMBER
-);
+-- -- INSERTION DES DONNEES DANS LA TABLE CHEESES
+-- INSERT INTO cheesess (id, fromage, family, paste, prix) 
+-- SELECT DISTINCT 
+--     REPLACE(TRIM(id || fromage || family || paste), ' ', '') AS id_concatenated, 
+--     fromage, 
+--     odsfromages.family, 
+--     paste, 
+--     price AS prix
+-- FROM ODSFROMAGES;
+-- -- creation de table temporaire vente 
+-- CREATE TABLE ventes (
+--     cheeses VARCHAR2(50),
+--     dates DATE,
+--     quantites NUMBER
+-- );
 ---- insertion des donnees dans la table ventes
-INSERT INTO DATES (DATEID, SALE_DATE) 
-SELECT DISTINCT ventes.dates AS DATEID,
-TO_DATE(ventes.dates, 'YYYY-MM-DD') AS SALE_DATE
-FROM VENTES;
+-- INSERT INTO DATES (DATEID, SALE_DATE) 
+-- SELECT DISTINCT ventes.dates AS DATEID,
+-- TO_DATE(ventes.dates, 'YYYY-MM-DD') AS SALE_DATE
+-- FROM VENTES;
 
 --insertion des cheess INSERT INTO cheeses (fromage, family, paste, prix)
 -- INSERT INTO cheesess (id,fromage, family, paste, prix)
@@ -78,39 +81,75 @@ SELECT
 FROM sales t
 JOIN cheesess c ON t.cheeses = c.family;
 
+----- afficher les donnees de la table ventes
 SELECT
-    v.id_ventes,
+    v.ventes_id,
     v.cheeses,
-    v.date_vente,
+    v.vente_date,
+    v.quantites
+
+    
+FROM
+    ventes v
+JOIN
+    cheesess c ON v.cheeses = c.family
+WHERE
+    c.prix IS NOT NULL;
+
+SELECT
+    v.ventes_id,
+    v.cheeses,
+    v.vente_date,
     v.quantites,
     c.prix,
     TO_NUMBER(c.prix)* (v.quantites) as Montant
 FROM
     ventes v
 JOIN
-    cheeses c ON v.cheeses = c.family
+    cheesess c ON v.cheeses = c.family
 WHERE
     REGEXP_LIKE(c.prix, '^-?\d+(\.\d+)?$');
 
------ motant total
+----- motant par ventes 
+
 SELECT
-    v.id_ventes,
+   v.ventes_id,
     v.cheeses,
-    v.date_vente,
+    v.vente_date,
     v.quantites,
-    c.prix,
-    TO_NUMBER(REGEXP_SUBSTR(c.prix, '^-?\d+(\.\d+)?')) AS prix_number,
+    c.prix as prix_unitaire,
     v.quantites * TO_NUMBER(REGEXP_SUBSTR(c.prix, '^-?\d+(\.\d+)?')) AS montant_total
 FROM
     ventes v
 JOIN
-    cheeses c ON v.cheeses = c.family
+    cheesess c ON v.cheeses = c.family
 WHERE
     REGEXP_LIKE(c.prix, '^-?\d+(\.\d+)?$');
 
 ------------------------------------------------------------
 --v
+--Le nombre de fromages par famille :
+SELECT
+    c.family,
+    COUNT(c.fromage) AS nombre_de_fromages
+FROM
+    cheesess c
+GROUP BY
+    c.family;
+----Le jour ayant réalisé le plus de chiffre d'affaires :
 
-
+--- LES 3 FROMAGE LE PLUS VENDU
+SELECT
+    c.fromage,
+    SUM(v.quantites) AS total_quantites_vendues
+FROM
+    ventes v
+JOIN
+    cheesess c ON v.cheeses = c.family
+GROUP BY
+    c.fromage
+ORDER BY
+    total_quantites_vendues DESC
+FETCH FIRST 3 ROWS ONLY;
 
 
